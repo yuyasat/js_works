2d7cd2de8279c01878abfcb472afc21243bee687
yuyataki <takiyour@gmail.com>
2017-02-04 12:40:27 +0900

    initial commit

---
+++ src/components/world.jsx
@@ -0,0 +1,9 @@
+// src/components/world.jsx
+import React from 'react';
+
+const World = () => {
+    return (
+        <h1>World</h1>
+    );
+}
+export default World;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: './src',
+    port: 5000
+  }
+}
